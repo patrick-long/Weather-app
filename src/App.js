67a01a31fd1a922462252ececeaa16fef9f1766c
longpatrick <long.patrick1997@gmail.com>
2021-05-18 15:16:07 -0500

    add dummy header links and styling

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <a href="/">H O M E</a>
+        <a href="#">S I G N &nbsp; U P</a>
+        <a href="#">L O G &nbsp; I N</a>
       </header>
     </div>
   );
